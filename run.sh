2d95b8f85497fed3b0bfbfb90832e96215dfd42f
Project3 <proj3@localhost.localdomain>
2017-12-16 12:38:09 -0500

    new file:   csapp.c 	new file:   csapp.h 	new file:   nameserver.cpp 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ dns_helper.cpp nameserver.cpp -o nameserver
+g++ csapp.c dns_helper.cpp nameserver.cpp -o nameserver
 ./nameserver "no" some_ip some_port topos/topo1/topo1.servers topos/topo1/topo1.lsa 
